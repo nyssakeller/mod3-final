b3207896d26f486b00e7940038dc1a2a7ca30feb
Nyssa Keller <nyssa1@gmail.com>
2018-02-22 10:27:33 -0700

    Test for new state in houseReducer

--- src/reducers/houseReducer.test.js
+++ src/reducers/houseReducer.test.js
@@ -1,4 +1,5 @@
-import * as actions from '../actions/index.js';
+// import * as actions from '../actions/index.js';
+import { setHouses } from '../actions/index.js';
 import { houseReducer } from './houseReducer.js';
 
 describe('houseReducer', () => {
@@ -6,6 +7,20 @@ describe('houseReducer', () => {
   it('return default state', () => {
     const expected = [];
     expect(expected).toEqual(houseReducer(undefined, {}));
+  });
+
+  it('return new state', () => {
+    const expected = [{
+      name: 'name',
+      founded: 'founded',
+      seats: 'seats',
+      titles: 'titles',
+      coatOfArms: 'coat of arms',
+      ancestralWeapons: 'weapons',
+      words: 'words'
+    }];
+
+    expect(houseReducer(undefined, setHouses(expected))).toEqual(expected);
   });
 
 });
